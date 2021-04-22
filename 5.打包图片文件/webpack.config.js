78c07ae760748d8308c2fe39a56d8be23323695e
fengqing <3451785715@qq.com>
2021-04-22 22:50:45 +0800

    增加注释

--- 5.打包图片文件/webpack.config.js
+++ 5.打包图片文件/webpack.config.js
@@ -5,33 +5,43 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'built.js',
-    path: resolve(__dirname, 'build'),
+    path: resolve(__dirname, 'build')
   },
   module: {
     rules: [
       {
         test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader'],
+        // 要使用多个 loader 处理用 use
+        use: ['style-loader', 'css-loader', 'less-loader']
       },
       {
+        // 问题：默认处理不了 html 中 img 图片
+        // 处理图片资源
         test: /\.(jpg|png|gif)$/,
         loader: 'url-loader',
         options: {
           limit: 74 * 1024,
+          // 问题：因为 url-loader 默认使用 es6 模块化解析，而 html-loader 引入图片是 commonjs
+          // 解析时会出问题：[object Module]
+          // 解决：关闭 url-loader 的 es6 模块化，使用 commonjs 解析
           esModule: false,
-          name: '[hash:10].[ext]',
-        },
+          // 给图片进行重命名
+          // [hash:10]取图片的 hash 的前 10 位
+          // [ext]取文件原来扩展名
+          name: '[hash:10].[ext]'
+        }
       },
       {
         test: /\.html$/,
-        loader: 'html-loader',
-      },
-    ],
+        // 处理 html 文件的 img 图片（负责引入 img，从而能被 url-loader 进行处理）
+        loader: 'html-loader'
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
+      template: './src/index.html'
+    })
   ],
-  mode: 'development',
+  mode: 'development'
 };
--- 6.打包其他资源/webpack.config.js
+++ 6.打包其他资源/webpack.config.js
@@ -5,24 +5,26 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'built.js',
-    path: resolve(__dirname, 'build'),
+    path: resolve(__dirname, 'build')
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
+      // 打包其他资源(除了 html/js/css 资源以外的资源)
       {
+        // 排除 css/js/html/png/jpg/gif/less/scss 资源
         exclude: /\.(html|css|js|png|jpg|gif|less|scss)$/,
-        loader: 'file-loader',
-      },
-    ],
+        loader: 'file-loader'
+      }
+    ]
   },
   plugins: [
     new htmlWebpackPlugin({
-      template: './src/index.html',
-    }),
+      template: './src/index.html'
+    })
   ],
-  mode: 'development',
+  mode: 'development'
 };
