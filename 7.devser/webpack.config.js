9ad0f2d57922915ba7745555b6beca2d6c2f341c
fengqing <3451785715@qq.com>
2021-04-23 16:44:26 +0800

    添加注释

--- 7.devser/webpack.config.js
+++ 7.devser/webpack.config.js
@@ -5,21 +5,25 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'js/built.js',
-    path: resolve(__dirname, 'build'),
+    path: resolve(__dirname, 'build')
   },
   module: {
-    rules: [],
+    rules: []
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
+      template: './src/index.html'
+    })
   ],
   mode: 'development',
   devServer: {
+    // 项目构建后路径
     contentBase: resolve(__dirname, 'build'),
+    // 启动 gzip 压缩
     compress: true,
+    // 端口号
     port: 3000,
-    open: true,
-  },
+    // 自动打开浏览器
+    open: true
+  }
 };
